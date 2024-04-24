3f5bceb752f57534faaee1a3314588064b90a7cf
Naikosito <163230533+Naikosito@users.noreply.github.com>
2024-04-24 19:41:26 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -53,7 +53,7 @@ void enlarge(HashMap * map) {
 
 HashMap * createMap(long capacity) {
     HashMap * map = (HashMap *)malloc(sizeof(HashMap));
-    map->buckets = (Pair **)malloc(sizeof(Pair *) * capacity);
+    map->buckets = (Pair **)calloc(sizeof(Pair *) * capacity);
     map->size = 0;
     map->capacity = capacity;
     map->current = -1; 
