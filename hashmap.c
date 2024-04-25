0513df0a3b38e55b4259c7512630f3b94881ebc8
Naikosito <163230533+Naikosito@users.noreply.github.com>
2024-04-25 00:32:24 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -141,7 +141,7 @@ Pair * firstMap(HashMap * map)
 Pair * nextMap(HashMap * map) 
 {  
   // al ser circular debemos asegurnarnos de que vuelva al principio en caso de llegar al tamaño de la tabla
-  int pos = (map->current + 1) % map->capacity;
+  int pos = map->current;
 
   
   while (map->buckets[pos] == NULL || map->buckets[pos]->key == NULL) 
